bff9c0554baf2e9ed6af78860ac43061ff38b093
Xiangyu Cai <xiangyu.cai@berkeley.edu>
2020-09-08 16:21:31 -0700

    fix .bat script

--- download_data.bat
+++ download_data.bat
@@ -1,5 +1,5 @@
-mkdir -p data
+mkdir data
 
-filename="easy_map_waypoints.txt"
-fileid="1QSOqGHWx2DtRvwl0kFwxQYidLVAZUZA7"
-curl -L  ${filename} "https://drive.google.com/uc?export=download&id=${fileid}" > data/${filename}
+set filename="easy_map_waypoints.txt"
+set fileid="1QSOqGHWx2DtRvwl0kFwxQYidLVAZUZA7"
+curl -L  %filename% "https://drive.google.com/uc?export=download&id=%fileid%" > data/%filename%
